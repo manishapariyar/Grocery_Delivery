1ab04a4c2e47ad5b24a778fca9a2c1307e28b0dc
manishapariyar <manishapariyar196@gmail.com>
2025-09-06 02:26:52 +0545

    refactor: reorganize imports and improve code formatting in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,52 +1,57 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRouter from './routes/authRouter.js';
-import session from 'express-session';
-import sellerRouter from './routes/sellerRouter.js';
-import productRouter from './routes/productRouter.js';
-import cartRouter from './routes/cartRouter.js';
-import addressRouter from './routes/addressRouter.js';
-import orderRouter from './routes/orderRouter.js';
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import authRouter from "./routes/authRouter.js";
+import sellerRouter from "./routes/sellerRouter.js";
+import productRouter from "./routes/productRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+import addressRouter from "./routes/addressRouter.js";
+import orderRouter from "./routes/orderRouter.js";
 
 const app = express();
 
+// __dirname fix for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-
-//middleware
-app.use(cors(
-  {
+// Middleware
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    method: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  }
-));
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(express.static('uploads'));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("uploads"));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "default_secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
+// API routes
+app.use("/api/auth/user", authRouter);
+app.use("/api/auth/seller", sellerRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
+// Serve React build folder
+app.use(express.static(path.join(__dirname, "build")));
 
+// Catch-all route for React Router
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-
-app.use(session({
-  secret: process.env.SESSION_SECRET ||
-    'default_secret_key',
-  resave: false,
-  saveUninitialized: true,
-
-}))
-
-
-app.get('/', (req, res) => {
-  res.status(200).send({ message: "Welcome to the E-commerce API" });
-})
-
-app.use('/api/auth/user', authRouter)
-app.use('/api/auth/seller', sellerRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/address', addressRouter);
-app.use('/api/order', orderRouter);
 export default app;
